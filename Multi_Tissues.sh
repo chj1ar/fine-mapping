c8a8e75650dd7d357ec6623a12e0b0e8c52895f9
chj1ar <chj1ar@163.com>
2019-09-01 19:13:02 +0800

    Formal pipeline. Only potential problem is multi-allelic case

--- Multi_Tissues.sh
+++ Multi_Tissues.sh
@@ -2,9 +2,12 @@
 
 echo "You should collect the Ensembl gene ID of all genes of interest into one file, and then pass this file as the argument of this script."
 
+echo "sos run Multi_tissues.ipynb preprocessing"
+sos run Multi_tissues.ipynb preprocessing
+
 for ensembl_gene_id in $(less $1) 
 do
-	echo "sos run Multi_tissues.ipynb --ensembl-gene-id $ensembl_gene_id"
-	sos run Multi_tissues.ipynb --ensembl-gene-id $ensembl_gene_id
+	echo "sos run Multi_tissues.ipynb extract --ensembl-gene-id $ensembl_gene_id"
+	sos run Multi_tissues.ipynb extract --ensembl-gene-id $ensembl_gene_id
 done
 
